bd85c15988c3e1ec899cececb8553da52b35746c
MoonStar <moonsung0331@gmail.com>
2023-12-14 18:30:54 +0900

    Local 환경을 위한 Config-Server 설정 및 Prod 환경 설정 분리

--- booking-service/src/main/java/com/example/bookingservice/RestDemo.java
+++ booking-service/src/main/java/com/example/bookingservice/RestDemo.java
@@ -1,26 +1,31 @@
 package com.example.bookingservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestDemo {
 
+    private Environment env;
+
     private final TrackingServiceClient trackingServiceClient;
 
     @Autowired
-    public RestDemo(TrackingServiceClient trackingServiceClient) {
+    public RestDemo(TrackingServiceClient trackingServiceClient, Environment env) {
         this.trackingServiceClient = trackingServiceClient;
-    }
-
-    @GetMapping("/")
-    public String getHi() {
-        return "Hi, Booking Service!";
+        this.env = env;
     }
 
     @GetMapping("/hello")
     public String getBookingMessage() {
         return "Welcome from Booking Application!" + trackingServiceClient.getHello();
+    }
+
+    @GetMapping("/hi")
+    public String getHi() {
+        return env.getProperty("greeting.message");
     }
 }
--- trracking-service/src/main/java/com/example/trrackingservice/DemoController.java
+++ trracking-service/src/main/java/com/example/trrackingservice/DemoController.java
@@ -1,13 +1,28 @@
 package com.example.trrackingservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    private Environment env;
+
+    @Autowired
+    public DemoController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/hello")
     public String getBookingMessage(){
         return "Welcome from Tracking Application!";
+    }
+
+    @GetMapping("/hi")
+    public String getHi() {
+        return env.getProperty("greeting.message");
     }
 }
